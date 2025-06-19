10322b88b1a3a6f21efbe237e10cc96ed8146dbe
italy <italy.christian@hotmail.com>
2025-06-19 10:17:02 -0300

    creating Menu and CountDown components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+
 // import { Heading } from "./components/Heading";
-// import { TimerIcon } from "lucide-react";
 import { Container } from "./components/Container";
+import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
+import { CountDown } from "./components/CountDown";
 
 export function App() {
   console.log("E ai");
@@ -10,7 +13,15 @@ export function App() {
   return (
     <>
       <Container>
-        <section>E AI</section>
+        <Logo />
+      </Container>
+
+      <Container>
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
