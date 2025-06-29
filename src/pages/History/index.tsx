87aa1521c35d62bcfd25a0afaa62e43dfbb04a5a
italy <italy.christian@hotmail.com>
2025-06-29 20:08:14 -0300

    adding dynamic infos into history table

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,11 +1,16 @@
+import styles from "./styles.module.css";
 import { TrashIcon } from "lucide-react";
 import { Button } from "../../components/Button";
 import { Container } from "../../components/Container";
 import { Heading } from "../../components/Heading";
 import { MainTemplate } from "../../Templates/MainTemplate";
-import styles from "./styles.module.css";
+import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { formatDate } from "../../utils/formatDate";
+import { getTaskStatus } from "../../utils/getTaskStatus";
 
 export const History = () => {
+  const { state } = useTaskContext();
+
   return (
     <MainTemplate>
       <Container>
@@ -36,14 +41,20 @@ export const History = () => {
             </thead>
 
             <tbody>
-              {Array.from({ length: 20 }).map((_, index) => {
+              {state.tasks.map((task) => {
+                const taskTypeDictionary = {
+                  workTime: "Foco",
+                  shortBreakTime: "Descanso curto",
+                  longBreakTime: "Descanso longo",
+                };
+
                 return (
-                  <tr key={index}>
-                    <td>Estudar</td>
-                    <td>25min</td>
-                    <td>20/04/2025 08:00</td>
-                    <td>Completa</td>
-                    <td>Foco</td>
+                  <tr key={task.id}>
+                    <td>{task.name}</td>
+                    <td>{task.duration}min</td>
+                    <td>{formatDate(task.startDate)}</td>
+                    <td>{getTaskStatus(task, state.activeTask)}</td>
+                    <td>{taskTypeDictionary[task.type]}</td>
                   </tr>
                 );
               })}
